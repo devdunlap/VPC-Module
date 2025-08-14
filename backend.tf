eb8afdad3ca776aeba2762f6bf1aaba070e68f75
devdunlap <dev.dunlap.andrew@gmail.com>
2025-08-13 20:44:57 -0500

    added enviornment specific state files

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "your-terraform-state-bucket"
-    key            = "terraform/state"
+    key            = "environments/${var.environment}/terraform.tfstate"
     region         = "us-west-2"
     encrypt        = true
     dynamodb_table = "terraform-state-lock"
