f562d2a6ead7d6b003ec1c8a2fdf83b5d0a56589
devdunlap <dev.dunlap.andrew@gmail.com>
2025-08-13 20:42:07 -0500

    added kms encryption key to secure state file bucket

--- backend.tf
+++ backend.tf
@@ -1,10 +1,14 @@
 terraform {
   backend "s3" {
-    bucket         = "your-s3-bucket-name"
+    bucket         = "your-terraform-state-bucket"
     key            = "terraform/state"
     region         = "us-west-2"
     encrypt        = true
-    dynamodb_table = "your-dynamodb-table-name"
+    dynamodb_table = "terraform-state-lock"
+    
+    # Additional security options
+    kms_key_id     = "arn:aws:kms:us-west-2:123456789012:key/12345678-1234-1234-1234-123456789012"
+    sse_algorithm  = "aws:kms"
   }
 }
 
